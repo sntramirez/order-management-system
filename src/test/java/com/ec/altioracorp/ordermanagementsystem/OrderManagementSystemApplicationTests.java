bb9a3d43a019bb7c010a04601f686e5296728481
Santiago Ramirez <snt.ramirez@gmail.com>
2024-06-11 15:31:02 -0500

    init

---
+++ src/test/java/com/ec/altioracorp/ordermanagementsystem/OrderManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ec.altioracorp.ordermanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
